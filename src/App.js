c750085d9336dd71d06b4f0aa97ffae5c0ce82ca
Andy <andrew.rutherford.6@gmail.com>
2020-05-10 16:47:18 +0700

    skeleton card is displayed while loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+import SkeletonCard from './components/SkeletonCard'
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setImages(data.hits);
+        setIsLoading(false);
+        }, 3000)
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  const searchText = (text) => {
+    setTerm(text);
+  };
+
   return (
-    <div className='App'>
-      <h1 className='text-6xl'>My app</h1>
+    <div className='container mx-auto'>
+      <ImageSearch searchText={searchText} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className='text-5xl text-center mx-auto mt-32'>No Images Found</h1>
+      )}
+
+      {isLoading ? (
+        <SkeletonCard />
+      ) : (
+        <div className='grid grid-cols-3 gap-4'>
+          {images &&
+            images.map((img) => <ImageCard key={img.id} image={img} />)}
+        </div>
+      )}
     </div>
   );
 }
